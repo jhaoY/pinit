be0499e72f5190c2d6ed08fa45d5f1ac4f6d8fa8
Justin Hao <justinhao@live.com>
2023-09-25 11:50:06 -0400

    Fix typos and incorrect syntax

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -4,9 +4,9 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255),
-  coverURL VARCHAR(255) SET DEFAULT 'https://cdn.pixabay.com/photo/2020/06/05/01/28/compass-5261062_1280.jpg',
-  location VARCHAR(255) SET DEFAULT 'Toronto, Ontario',
+  coverURL VARCHAR(255) DEFAULT 'https://cdn.pixabay.com/photo/2020/06/05/01/28/compass-5261062_1280.jpg',
+  location VARCHAR(255) DEFAULT 'Toronto, Ontario',
   created_by INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  times_favorited INTEGER SET DEFAULT 0,
-  deleted BOOLEAN SET DEFAULT FALSE
+  times_favorited INTEGER DEFAULT 0,
+  deleted BOOLEAN DEFAULT FALSE
 );
