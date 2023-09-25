b1fa53d2fe176bef01da3ecc3f5e3de0605e035c
Justin Hao <justinhao@live.com>
2023-09-25 15:42:00 -0400

    Add queries to get user maps

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -1,12 +1,59 @@
+const { query } = require('express');
 const db = require('../connection');
 
 const getMaps = () => {
   return db.query('SELECT * FROM maps')
     .then(data => {
-      console.log(data.rows);
       return data.rows;
     })
-    .catch(err => {console.error(err)});
+    .catch(err => {
+      console.error(err);
+      throw err;
+    })
 }
 
-getMaps();
+const getUserMaps = (id) => {
+  const queryText = `
+  SELECT * 
+  FROM maps
+  WHERE user_id = $1;
+  `;
+
+  return db.query(queryText, [id])
+}
+
+const getUserFavoriteMaps = (id) => {
+  const queryText = `
+  SELECT * 
+  FROM favorites
+  WHERE user_id = $1;
+  `;
+
+  return db.query(queryText, [id])
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      console.error(err);
+      throw err;
+    })
+};
+
+const getUserContributionMaps = (id) => {
+  const queryText = `
+  SELECT *
+  FROM contributions
+  WHERE user_id = $1;
+  `;
+
+  return db.query(queryText, [id])
+    .then(data => {
+      return data.rows;
+    })
+    .catch(err => {
+      console.error(err);
+      throw err;
+    })
+};
+
+module.export = { getMaps, getUserFavoriteMaps, getUserMaps, getUserContributionMaps }
