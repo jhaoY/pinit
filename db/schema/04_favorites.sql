bb49749d10ffefc2b68c8f140bea198d16629da2
RomelDonovan <RomelDonovan@live.ca>
2023-09-25 20:26:59 +0000

    Pull from master seed and database

---
+++ db/schema/04_favorites.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS favorites;
+
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) NOT NULL,
+  map_id INTEGER REFERENCES maps(id) NOT NULL
+);
