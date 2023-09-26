fa7a411289cdaba48ad6b9161d98d916e085c99f
Fahad Kamal <fahadkamal@hotmail.com>
2023-09-26 10:27:11 -0400

    add route to get data from map db

--- routes/maps_all.js
+++ routes/maps_all.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router  = express.Router();
+const { getMaps } = require('../db/queries/maps');
 
 router.get('/', (req, res) => {
   res.render('maps_all');
+});
+
+router.get('/', (req, res) => {
+  const userId = 1;
+  Promise.all(
+  getMaps(userId)
+  )
+    .then((maps) => {
+      res.render('profile', { maps });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Server error");
+    });
 });
 
 module.exports = router;
