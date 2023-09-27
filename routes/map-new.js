61ec2cf3293f36693710875a1dfded1e7cff315d
RomelDonovan <RomelDonovan@live.ca>
2023-09-27 13:42:41 +0000

    add router.post

--- routes/map-new.js
+++ routes/map-new.js
@@ -4,5 +4,9 @@ const router  = express.Router();
 router.get('/', (req, res) => {
   res.render('map_new');
 });
+router.post('/', (req, res) => {
+  console.log(req.body)
+});
+
 
 module.exports = router;
