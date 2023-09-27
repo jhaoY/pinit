fef1671c317a6c7420bfd2e652fa5e64412470f9
Justin Hao <justinhao@live.com>
2023-09-27 13:45:17 -0400

    Move map to jquery and Fix init map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,46 @@
-// Client facing scripts here
+$(document).ready(function () {
+  let map;
+
+  const initMap = (latitude, longitude) => {
+    map = L.map('map').setView([43.651070, -79.347015], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([43.651070, -79.347015]).addTo(map)
+    // .bindPopup().openPopup();
+  }
+
+  const moveMap = (latitude, longitude) => {
+    map.panTo(new L.LatLng(latitude, longitude), 13);
+  }
+
+  const getLocation = () => {
+    const info = document.getElementById('city').value;
+    const apiUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(info)}&format=json`;
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          const latitude = parseFloat(data[0].lat);
+          const longitude = parseFloat(data[0].lon);
+
+          // Move the map with the obtained coordinates
+          moveMap(latitude, longitude);
+        } else {
+          alert('Location not found!');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  $('#updateLocationBtn').on('click', function () {
+    getLocation();
+  })
+
+  // Call function to show default location
+  initMap();
+})
