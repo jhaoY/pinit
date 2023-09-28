8cad19c9ad1eed7fa78839d70f00d32ac55229d2
Justin <justinhao@live.com>
2023-09-28 14:53:26 -0400

    Change table and seeds to use lat/lng

--- db/schema/03_pins.sql
+++ db/schema/03_pins.sql
@@ -4,7 +4,8 @@ CREATE TABLE pins (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   description VARCHAR(255),
-  address VARCHAR(255) NOT NULL,
+  lat DOUBLE PRECISION NOT NULL,
+  lng DOUBLE PRECISION NOT NULL,
   map_id INTEGER REFERENCES maps(id) NOT NULL,
   imageURL VARCHAR(255) DEFAULT 'https://cdn.pixabay.com/photo/2023/08/16/18/05/homes-8194751_1280.png',
   deleted BOOLEAN DEFAULT false
--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -1,10 +1,10 @@
-INSERT INTO pins (title, description, address, map_id, imageURL)
+INSERT INTO pins (title, description, lat, lng, map_id, imageURL)
 VALUES
-('Central Park', 'A beautiful park in the heart of New York City', 'Central Park, New York, NY', 1, 'https://www.centralparknyc.org/images/default-source/default-album/homepage__park_view.jpg?sfvrsn=98b84d71_8'),
-('Empire State Building', 'Iconic skyscraper in Manhattan', '350 5th Ave, New York, NY', 1, 'https://www.esbnyc.com/sites/default/files/styles/downloadable_360x240/public/2019-09/ESB%20FIREWORKS%20WEBSITE%20PAGE.jpg?itok=s2bY6SHe'),
-('Statue of Liberty', 'A symbol of freedom and democracy', 'Liberty Island, New York, NY', 1, 'https://www.nps.gov/stli/learn/historyculture/images/Lady-Liberty-stands-watch.jpg'),
-('Times Square', 'The crossroads of the world', 'Times Square, New York, NY', 1, 'https://media.istockphoto.com/photos/times-square-new-york-picture-id865830240?s=2048x2048'),
-('CN Tower', 'Iconic tower in Toronto', '301 Front St W, Toronto, ON', 2, 'https://media.istockphoto.com/photos/cn-tower-picture-id536161788?s=2048x2048'),
-('Ripley''s Aquarium', 'Underwater wonders in Toronto', '288 Bremner Blvd, Toronto, ON', 2, 'https://media.istockphoto.com/photos/coral-reef-in-ripleys-aquarium-picture-id1025790474?s=2048x2048'),
-('Washington Monument', 'Iconic obelisk in Washington, D.C.', '2 15th St NW, Washington, D.C.', 3, 'https://cdn.pixabay.com/photo/2017/11/16/00/44/washington-dc-2957029_1280.jpg'),
-('The White House', 'Official residence and workplace of the President of the United States', '1600 Pennsylvania Ave NW, Washington, D.C.', 3, 'https://cdn.pixabay.com/photo/2015/08/27/09/06/ballroom-909977_1280.jpg');
+('Central Park', 'A beautiful park in the heart of New York City', 40.785091, -73.968285, 1, 'https://www.centralparknyc.org/images/default-source/default-album/homepage__park_view.jpg?sfvrsn=98b84d71_8'),
+('Empire State Building', 'Iconic skyscraper in Manhattan', 40.748817, -73.985428, 1, 'https://www.esbnyc.com/sites/default/files/styles/downloadable_360x240/public/2019-09/ESB%20FIREWORKS%20WEBSITE%20PAGE.jpg?itok=s2bY6SHe'),
+('Statue of Liberty', 'A symbol of freedom and democracy', 40.689247, -74.044502, 1, 'https://www.nps.gov/stli/learn/historyculture/images/Lady-Liberty-stands-watch.jpg'),
+('Times Square', 'The crossroads of the world', 40.758896, -73.985130, 1, 'https://media.istockphoto.com/photos/times-square-new-york-picture-id865830240?s=2048x2048'),
+('CN Tower', 'Iconic tower in Toronto', 43.642566, -79.387057, 2, 'https://media.istockphoto.com/photos/cn-tower-picture-id536161788?s=2048x2048'),
+('Ripley''s Aquarium', 'Underwater wonders in Toronto', 43.642403, -79.386594, 2, 'https://media.istockphoto.com/photos/coral-reef-in-ripleys-aquarium-picture-id1025790474?s=2048x2048'),
+('Washington Monument', 'Iconic obelisk in Washington, D.C.', 38.889484, -77.035279, 3, 'https://cdn.pixabay.com/photo/2017/11/16/00/44/washington-dc-2957029_1280.jpg'),
+('The White House', 'Official residence and workplace of the President of the United States', 38.897676, -77.036530, 3, 'https://cdn.pixabay.com/photo/2015/08/27/09/06/ballroom-909977_1280.jpg');
