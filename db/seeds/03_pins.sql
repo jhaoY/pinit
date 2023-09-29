b730cd0e451a4268f219272a4e802d609af9a42d
RomelDonovan <romeldonovan@live.ca>
2023-09-28 22:48:30 -0400

    Change fake data seed from delete = true to false

--- db/seeds/03_pins.sql
+++ db/seeds/03_pins.sql
@@ -2,7 +2,7 @@ INSERT INTO pins (title, description, lat, lng, map_id, imageURL, deleted)
 VALUES
 ('Central Park', 'A beautiful park in the heart of New York City', 40.785091, -73.968285, 1, 'https://www.centralparknyc.org/images/default-source/default-album/homepage__park_view.jpg?sfvrsn=98b84d71_8', false),
 ('Empire State Building', 'Iconic skyscraper in Manhattan', 40.748817, -73.985428, 1, 'https://www.esbnyc.com/sites/default/files/styles/downloadable_360x240/public/2019-09/ESB%20FIREWORKS%20WEBSITE%20PAGE.jpg?itok=s2bY6SHe', false),
-('Statue of Liberty', 'A symbol of freedom and democracy', 40.689247, -74.044502, 1, 'https://www.nps.gov/stli/learn/historyculture/images/Lady-Liberty-stands-watch.jpg', true),
+('Statue of Liberty', 'A symbol of freedom and democracy', 40.689247, -74.044502, 1, 'https://www.nps.gov/stli/learn/historyculture/images/Lady-Liberty-stands-watch.jpg', false),
 ('Times Square', 'The crossroads of the world', 40.758896, -73.985130, 1, 'https://media.istockphoto.com/photos/times-square-new-york-picture-id865830240?s=2048x2048', false),
 ('CN Tower', 'Iconic tower in Toronto', 43.642566, -79.387057, 2, 'https://media.istockphoto.com/photos/cn-tower-picture-id536161788?s=2048x2048', false),
 ('Ripley''s Aquarium', 'Underwater wonders in Toronto', 43.642403, -79.386594, 2, 'https://media.istockphoto.com/photos/coral-reef-in-ripleys-aquarium-picture-id1025790474?s=2048x2048', false),
